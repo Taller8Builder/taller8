a7827f4162337d85a3c91bc8899c65f7c05751ec
Fabrizio GC <milton.garcia1998@hotmail.com>
2018-12-11 12:46:32 -0500

    Clases agregadas

---
+++ ManejoComputadoras/src/emsamablajecomputadoras/creacional/ComputadorDirector.java
@@ -0,0 +1,11 @@
+package emsamablajecomputadoras.creacional;
+
+public class ComputadorDirector {
+	private ComputadorBuilder builder;
+	
+	public ComputadorDirector(ComputadorBuilder builder) {
+		this.builder = builder;
+	}
+	
+	public void 
+}
